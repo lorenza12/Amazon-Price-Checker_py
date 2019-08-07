a6c0cf1d75894e8e34048f9367716d36d229af86
Alik <38536682+lorenza12@users.noreply.github.com>
2019-08-07 10:31:38 -0500

    Adding Price Checker Manual File
    Adding main program file that can be ran manually for testing purposes

---
+++ Price_Checker_Manual.py
@@ -0,0 +1,94 @@
+import schedule
+import time
+import Scraper
+import CSV_Helper
+import Helper_Functions
+import Config
+
+
+
+def main():
+    
+    try:
+        csv_out = []
+
+        csv_list = CSV_Helper.open_csv_file(Config.csv_directory)
+        header = csv_list[0]
+
+        #start building the data to write out including the header
+        csv_out.append(header)
+
+
+        for item in csv_list[1:]:
+            #URL, Title, Current Price, Purcahse Price
+            item_list = []
+
+            item_title = None
+            current_price = None
+            purchase_price = None
+
+            #amazon.com sometimes thinks the call is from a robot.. ;) so we can
+            #switch up our request headers to trick them in thinking it is normal use
+            header_counter = 1
+
+            #information from the csv file
+            url = item[0]
+            current_title = item[1]
+            current_price = item[2]
+            purchase_price = item[3]
+            
+            if url != None and url != '':
+                while item_title == None and header_counter < 11:
+                    response_html = Scraper.request_html(url, header_counter)
+
+                    item_title = Scraper.get_title(response_html)
+                    item_price = Scraper.get_price(response_html)
+                    item_sale_price = Scraper.get_deal_price(response_html)
+
+                    #if amazon thought we were a robot, try a different header fromat
+                    header_counter += 1
+
+                if item_title == None or item_price == None:
+                    #couldn't get html but still need to rewrite the unchanged data to the csv
+                    print('Unable to get html')
+
+                    item_list.extend((url, current_title, current_price, purchase_price))
+
+                else:
+                    #we were able to get html back
+                    
+                    updated_title = Helper_Functions.compare_titles(item_title, current_title)
+                    updated_price = Helper_Functions.compare_sale_price(Helper_Functions.compare_current_prices(item_price, current_price), item_sale_price)
+
+                    #create list to update csv file
+                    #if the titles or prices we got back in html happen to be blank, write out the old values that were already there
+                    item_list.extend((url, 
+                                    (updated_title if updated_title != None or updated_title == '' else current_title), 
+                                    (updated_price if updated_price != None or updated_price == '' else current_price), 
+                                    purchase_price))
+
+                    should_buy = Helper_Functions.compare_purchase_price(updated_price, purchase_price, url, updated_title)
+
+                    if should_buy:
+                        #send email to buy
+                        
+                        Helper_Functions.purchase_notification(item_title, updated_price, url)
+
+                    csv_out.append(item_list)
+        
+
+        #Write the file
+        CSV_Helper.write_csv_file(csv_out, Config.csv_directory)
+        
+    except Exception as e:
+        
+        message = f'Fatal! main: {str(e)} \n'
+        Helper_Functions.write_log(message,0)
+        input('+ Press Enter to Exit')
+
+
+if __name__ == "__main__":
+    print('+ Starting Application...')
+    main()
+    print('+ Application Complete...')
+    input('+ Press Enter to Exit')
